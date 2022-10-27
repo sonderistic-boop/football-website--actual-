35d580c8aa415297331251ae8488d88f9c245f7a
joerock7 <joerockmcfc7@gmail.com>
2022-10-27 23:39:26 +0100

    created forms and key

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/login")
-def login():
-    return render_template('login.html', title = 'Login')
+app.config['SECRET_KEY'] = '4138d469444dd804d57db2c3cb47026b'
 
+@app.route("/login")
+    
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form=form)
+
+@app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', title = 'Home')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form=form)
     
 
 if __name__ == '__main__':
